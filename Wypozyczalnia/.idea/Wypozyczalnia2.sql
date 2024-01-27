d336db9eae6379a3832d94eb10ef44bf8c390ab2
s26964-pj <115402943+s26964-pj@users.noreply.github.com>
2024-01-27 09:44:32 +0100

    push

---
+++ Wypozyczalnia/.idea/Wypozyczalnia2.sql
@@ -0,0 +1,289 @@
+-- Klienci
+
+SELECT * FROM Klienci;
+
+SELECT KlientID, Imie, Nazwisko, DataUr
+FROM Klienci
+ORDER BY Nazwisko ASC, Imie DESC;
+
+SELECT Klienci.Imie, Klienci.Nazwisko, Wypozyczenie.DaataOd
+FROM Klienci
+INNER JOIN Wypozyczenie ON Klienci.KlientID = Wypozyczenie.KlientID
+ORDER BY Wypozyczenie.DaataOd DESC;
+
+UPDATE Klienci
+SET NumerTel = '555000111'
+WHERE Nazwisko = 'Wójcik';
+
+UPDATE Klienci
+SET Nazwisko = 'Nowacki'
+WHERE DataUr < '1990-01-01';
+
+DELETE FROM Klienci WHERE KlientID = 3;
+
+DELETE FROM Klienci WHERE NumerTel LIKE '555%';
+
+ALTER TABLE Klienci
+ADD CONSTRAINT unique_pesel UNIQUE (Pesel);
+
+SELECT Imie, Nazwisko, DataUr AS DataUrodzenia
+FROM Klienci;
+
+SELECT COUNT(*) FROM Klienci;
+
+SELECT Imie, Nazwisko, MIN(DataUr) as NajmlodszyKlient
+FROM Klienci;
+
+-- Pracownicy
+
+SELECT *
+FROM Pracownicy
+WHERE Stanowisko NOT IN ('Recepcjonistka', 'Księgowa');
+
+SELECT Klienci.Imie, Klienci.Nazwisko
+FROM Klienci
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Wypozyczenie
+    WHERE Wypozyczenie.KlientID = Klienci.KlientID
+);
+UPDATE Pracownicy
+SET Stanowisko = 'Specjalista ds. Klientów'
+WHERE PracownikID = 3;
+
+UPDATE Pracownicy
+SET NumerTel = '123456777'
+WHERE NumerTel LIKE '%777';
+
+DELETE FROM Pracownicy WHERE PracownikID = 2;
+
+DELETE FROM Pracownicy WHERE Stanowisko = 'Recepcjonistka';
+
+SELECT Stanowisko, AVG(LENGTH(NumerTel)) as SredniaDlugoscNumeru
+FROM Pracownicy
+GROUP BY Stanowisko
+HAVING AVG(LENGTH(NumerTel)) > 8;
+
+ALTER TABLE Pracownicy
+ADD CONSTRAINT unique_numer_tel UNIQUE (NumerTel);
+
+SELECT Imie, Nazwisko, Stanowisko AS PracowniczeStanowisko
+FROM Pracownicy;
+
+SELECT SUM(LENGTH(NumerTel)) as SumaDlugosciNumerow FROM Pracownicy;
+
+-- Samochody
+
+SELECT *
+FROM Samochody
+WHERE RokProd BETWEEN 2017 AND 2020;
+
+SELECT SamochodID, Marka, Model, RokProd
+FROM Samochody
+WHERE RokProd < 2020;
+
+UPDATE Samochody
+SET RokProd = 2019
+WHERE SamochodID = 2;
+
+UPDATE Samochody
+SET Silnik = 'Benzyna'
+WHERE RokProd < 2020;
+
+DELETE FROM Samochody WHERE SamochodID = 5;
+
+DELETE FROM Samochody WHERE Silnik = 'Diesel';
+
+SELECT Silnik, COUNT(*) as LiczbaSamochodow
+FROM Samochody
+GROUP BY Silnik
+HAVING COUNT(*) > 1;
+
+ALTER TABLE Samochody
+ADD CONSTRAINT unique_model UNIQUE (Model);
+
+SELECT Marka, Model AS NazwaModelu
+FROM Samochody;
+
+SELECT MAX(RokProd) as NajnowszyRokProdukcji FROM Samochody;
+
+-- Lokalizacje
+
+SELECT * FROM Lokalizacje;
+
+SELECT LokalizacjaID, Miasto, Panstwo
+FROM Lokalizacje
+WHERE Miasto LIKE 'W%';
+
+UPDATE Lokalizacje
+SET KodPocztowy = '50-005'
+WHERE LokalizacjaID = 4;
+
+UPDATE Lokalizacje
+SET Panstwo = 'Poland'
+WHERE Panstwo = 'Polska';
+
+DELETE FROM Lokalizacje WHERE LokalizacjaID = 2;
+
+DELETE FROM Lokalizacje WHERE Miasto LIKE 'W%';
+
+SELECT Panstwo, COUNT(*) as LiczbaOddzialow
+FROM Oddzialy
+JOIN Lokalizacje ON Oddzialy.LokalizacjaID = Lokalizacje.LokalizacjaID
+GROUP BY Panstwo
+HAVING COUNT(*) > 1;
+
+ALTER TABLE Lokalizacje
+ADD CONSTRAINT unique_kod_pocztowy UNIQUE (KodPocztowy);
+
+SELECT Miasto, Panstwo AS NazwaPanstwa
+FROM Lokalizacje;
+
+SELECT COUNT(DISTINCT Panstwo) as LiczbaKrajow FROM Lokalizacje;
+
+-- Platnosci
+
+SELECT * FROM Platnosci;
+
+SELECT PlatnoscID, Kwota, TypPlatnosci
+FROM Platnosci
+WHERE Kwota > 100;
+
+UPDATE Platnosci
+SET Kwota = 150.00
+WHERE PlatnoscID = 3;
+
+UPDATE Platnosci
+SET TypPlatnosci = 'Karta'
+WHERE TypPlatnosci = 'Gotówka';
+
+DELETE FROM Platnosci WHERE PlatnoscID = 7;
+
+DELETE FROM Platnosci WHERE TypPlatnosci = 'Kartą';
+
+SELECT TypPlatnosci, AVG(Kwota) as SredniaKwota
+FROM Platnosci
+GROUP BY TypPlatnosci
+HAVING AVG(Kwota) > 90.00;
+
+ALTER TABLE Platnosci
+ADD CONSTRAINT unique_typ_platnosci UNIQUE (TypPlatnosci);
+
+SELECT Kwota, TypPlatnosci AS RodzajPlatnosci
+FROM Platnosci;
+
+SELECT SUM(Kwota) as SumaKwot FROM Platnosci;
+
+-- Oddzialy
+
+SELECT * FROM Oddzialy;
+
+SELECT OddzialID, NazwaOddzialu, NumerTelOddzialu
+FROM Oddzialy
+WHERE NumerTelOddzialu LIKE '%111%';
+
+UPDATE Oddzialy
+SET NazwaOddzialu = 'Oddział Gdańsk-Nowy'
+WHERE OddzialID = 5;
+
+UPDATE Oddzialy
+SET NumerTelOddzialu = '111222333'
+WHERE LokalizacjaID = 2;
+
+DELETE FROM Oddzialy WHERE OddzialID = 3;
+
+DELETE FROM Oddzialy WHERE PracownikID IS NULL;
+
+SELECT NazwaOddzialu, COUNT(*) as LiczbaPracownikow
+FROM Pracownicy
+JOIN Oddzialy ON Pracownicy.PracownikID = Oddzialy.PracownikID
+GROUP BY NazwaOddzialu
+HAVING COUNT(*) > 1;
+
+ALTER TABLE Oddzialy
+ADD CONSTRAINT unique_numer_tel_oddzialu UNIQUE (NumerTelOddzialu);
+
+SELECT NazwaOddzialu, NumerTelOddzialu AS KontaktOddzialu
+FROM Oddzialy;
+
+SELECT AVG(LENGTH(NazwaOddzialu)) as SredniaDlugoscNazwy FROM Oddzialy;
+
+-- Wypozyczenie
+
+SELECT *
+FROM Wypozyczenie
+WHERE KlientID IN (2, 4, 6);
+
+SELECT WypozyczenieID, DaataOd, DataDo
+FROM Wypozyczenie
+WHERE DataDo > '2023-11-20';
+
+UPDATE Wypozyczenie
+SET DataDo = '2023-11-25'
+WHERE WypozyczenieID = 7;
+
+UPDATE Wypozyczenie
+SET KlientID = 2
+WHERE DaataOd < '2023-11-10';
+
+DELETE FROM Wypozyczenie WHERE WypozyczenieID = 6;
+
+DELETE FROM Wypozyczenie WHERE DaataOd < '2023-11-10';
+
+SELECT KlientID, COUNT(*) as LiczbaWypozyczen
+FROM Wypozyczenie
+GROUP BY KlientID
+HAVING COUNT(*) > 2;
+
+ALTER TABLE Wypozyczenie
+ADD CONSTRAINT unique_data_od UNIQUE (DaataOd);
+
+SELECT WypozyczenieID, DaataOd AS DataRozpoczeciaWypozyczenia
+FROM Wypozyczenie;
+
+SELECT WypozyczenieID, DATEDIFF(DataDo, DaataOd) as RoznicaDni
+FROM Wypozyczenie;
+
+-- SamochodWypozyczenie
+
+SELECT * FROM SamochodWypozyczenie;
+
+SELECT SamochodWypozyczenie, SamochodID
+FROM SamochodWypozyczenie
+WHERE WypozyczenieID = 2;
+
+UPDATE SamochodWypozyczenie
+SET SamochodID = 3
+WHERE SamochodWypozyczenie = 12;
+
+UPDATE SamochodWypozyczenie
+SET WypozyczenieID = 8
+WHERE SamochodID = 6;
+
+DELETE FROM SamochodWypozyczenie WHERE SamochodWypozyczenie = 5;
+
+DELETE FROM SamochodWypozyczenie WHERE SamochodID = 6;
+
+SELECT WypozyczenieID, COUNT(*) as LiczbaSamochodow
+FROM SamochodWypozyczenie
+GROUP BY WypozyczenieID
+HAVING COUNT(*) > 1;
+
+ALTER TABLE SamochodWypozyczenie
+ADD CONSTRAINT unique_samochod_id UNIQUE (SamochodID);
+
+SELECT SamochodWypozyczenie AS IDWypozyczenia, SamochodID AS IDSamochodu
+FROM SamochodWypozyczenie;
+
+SELECT SamochodID, COUNT(DISTINCT WypozyczenieID) as LiczbaWypozyczen
+FROM SamochodWypozyczenie
+GROUP BY SamochodID;
+
+-- Truncate
+
+TRUNCATE TABLE wypozyczenie;
+
+TRUNCATE TABLE klienci;
+
+
+
--- Wypozyczalnia/Wypozyczalnia1.sql
+++ Wypozyczalnia/Wypozyczalnia1.sql
@@ -1,125 +1,183 @@
--- Update
+CREATE TABLE Klienci(
+    `KlientID` int not null,
+    `Imie` varchar(20),
+    `Nazwisko` varchar(20),
+    `Pesel` varchar(11),
+    `DataUr` date,
+    `NumerTel` varchar(9) CHECK (LENGTH(`NumerTel`) = 9),
+    primary key(KlientID));
 
--- Klienci
+CREATE TABLE Pracownicy(
+    `PracownikID` int not null,
+    `Imie` varchar(20),
+    `Nazwisko` varchar(20),
+    `Stanowisko` varchar(40),
+    `NumerTel` varchar(9),
+    primary key(PracownikID));
 
-UPDATE Klienci
-SET NumerTel = '555000111'
-WHERE Nazwisko = 'Wójcik';
+Create TABLE Samochody(
+    `SamochodID` int not null,
+    `Marka` varchar(20),
+    `Model` varchar(20),
+    `RokProd` year,
+    `Silnik` enum('Benzyna', 'Diesel'),
+    primary key (SamochodID));
 
-UPDATE Klienci
-SET Nazwisko = 'Nowacki'
-WHERE DataUr < '1990-01-01';
+CREATE TABLE Lokalizacje(
+    `LokalizacjaID` int not null,
+    `Miasto` varchar(20),
+    `Adres` varchar(20),
+    `KodPocztowy` varchar(6),
+    `Panstwo` varchar(20),
+    primary key(LokalizacjaID));
 
--- Pracownicy
+CREATE TABLE Platnosci(
+    `PlatnoscID` int not null,
+    `TypPlatnosci` enum('Kartą', 'Gotówka'),
+    `Kwota` double,
+    `Data` date,
+    primary key(PlatnoscID));
 
-UPDATE Pracownicy
-SET Stanowisko = 'Specjalista ds. Klientów'
-WHERE PracownikID = 3;
+CREATE TABLE Oddzialy(
+    `OddzialID` int not null,
+    `LokalizacjaID` int,
+    `PracownikID` int,
+    `NazwaOddzialu` varchar(20),
+    `NumerTelOddzialu` varchar(9),
+    primary key(OddzialID),
+    foreign key (LokalizacjaID) REFERENCES Lokalizacje(LokalizacjaID),
+    foreign key (PracownikID) REFERENCES Pracownicy(PracownikID));
 
-UPDATE Pracownicy
-SET NumerTel = '123456777'
-WHERE NumerTel LIKE '%777';
+CREATE TABLE Wypozyczenie(
+    `WypozyczenieID` int not null,
+    `KlientID` int,
+    `PlatnoscID` int,
+    `OddzialID` int,
+    `DaataOd` date,
+    `DataDo` date,
+    primary key(WypozyczenieID),
+    foreign key(PlatnoscID) REFERENCES Platnosci(PlatnoscID),
+    foreign key(OddzialID) REFERENCES Oddzialy(OddzialID),
+    foreign key(KlientID) REFERENCES Klienci(KlientID));
 
--- Samochody
+CREATE TABLE SamochodWypozyczenie(
+    `SamochodWypozyczenie` int not null,
+    `WypozyczenieID` int,
+    `SamochodID` int,
+    primary key(SamochodWypozyczenie),
+    foreign key(WypozyczenieID) REFERENCES Wypozyczenie(WypozyczenieID),
+    foreign key(SamochodID) REFERENCES Samochody(SamochodID));
 
-UPDATE Samochody
-SET RokProd = 2019
-WHERE SamochodID = 2;
+INSERT INTO Klienci (`KlientID`, `Imie`, `Nazwisko`, `Pesel`, `DataUr`, `NumerTel`)
+VALUES
+(1, 'Jan', 'Kowalski', '12345678901', '1990-05-15', '123456789'),
+(2, 'Anna', 'Nowak', '23456789012', '1985-08-21', '987654321'),
+(3, 'Piotr', 'Duda', '34567890123', '1978-03-10', '555444333'),
+(4, 'Karolina', 'Mazur', '45678901234', '1992-11-25', '111222333'),
+(5, 'Marcin', 'Nowacki', '56789012345', '1983-07-05', '999888777'),
+(6, 'Katarzyna', 'Kaczor', '67890123456', '1995-01-30', '777666555'),
+(7, 'Robert', 'Wójcik', '78901234567', '1987-09-17', '333222111'),
+(8, 'Agnieszka', 'Michalak', '89012345678', '1980-12-03', '444555666'),
+(9, 'Mateusz', 'Mazurek', '90123456789', '1991-06-20', '666777888'),
+(10, 'Weronika', 'Makowska', '01234567890', '1994-04-12', '222333444');
 
-UPDATE Samochody
-SET Silnik = 'Benzyna'
-WHERE RokProd < 2020;
+INSERT INTO Samochody (`SamochodID`, `Marka`, `Model`, `RokProd`, `Silnik`)
+VALUES
+(1, 'Volkswagen', 'Golf', 2015, 'Diesel'),
+(2, 'Ford', 'Focus', 2018, 'Benzyna'),
+(3, 'Toyota', 'Corolla', 2020, 'Benzyna'),
+(4, 'Opel', 'Astra', 2017, 'Diesel'),
+(5, 'Renault', 'Clio', 2016, 'Benzyna'),
+(6, 'BMW', '3 Series', 2019, 'Diesel'),
+(7, 'Audi', 'A4', 2018, 'Diesel'),
+(8, 'Mercedes-Benz', 'C-Class', 2021, 'Benzyna'),
+(9, 'Hyundai', 'i30', 2022, 'Benzyna'),
+(10, 'Skoda', 'Octavia', 2020, 'Diesel');
 
--- Lokalizacje
+INSERT INTO Lokalizacje (`LokalizacjaID`, `Miasto`, `Adres`, `KodPocztowy`, `Panstwo`)
+VALUES
+(1, 'Warszawa', 'ul. Wiejska 1', '00-001', 'Polska'),
+(2, 'Kraków', 'ul. Rynek 2', '30-002', 'Polska'),
+(3, 'Gdańsk', 'ul. Portowa 3', '80-003', 'Polska'),
+(4, 'Wrocław', 'ul. Plac Solny 4', '50-004', 'Polska'),
+(5, 'Poznań', 'ul. Stary Rynek 5', '60-005', 'Polska'),
+(6, 'Łódź', 'ul. Piotrkowska 6', '90-006', 'Polska'),
+(7, 'Katowice', 'ul. Mariacka 7', '40-007', 'Polska'),
+(8, 'Szczecin', 'ul. Waly Chrobrego 8', '70-008', 'Polska'),
+(9, 'Gdynia', 'ul. Kosciuszki 9', '81-009', 'Polska'),
+(10, 'Bydgoszcz', 'ul. Dworcowa 10', '85-010', 'Polska');
 
-UPDATE Lokalizacje
-SET KodPocztowy = '50-005'
-WHERE LokalizacjaID = 4;
+INSERT INTO Pracownicy (`PracownikID`, `Imie`, `Nazwisko`, `Stanowisko`, `NumerTel`)
+VALUES
+(1, 'Mariusz', 'Nowak', 'Manager', '111222333'),
+(2, 'Alicja', 'Kowalska', 'Recepcjonistka', '444555666'),
+(3, 'Krzysztof', 'Duda', 'Konsultant ds. Wypożyczeń', '777888999'),
+(4, 'Ewa', 'Mazurek', 'Księgowa', '555666777'),
+(5, 'Adam', 'Michalak', 'Mechanik', '888999000'),
+(6, 'Patryk', 'Kaczor', 'Konsultant ds. Samochodów', '222333444'),
+(7, 'Martyna', 'Wójcik', 'Konsultant ds. Klientów', '999000111'),
+(8, 'Paweł', 'Makowski', 'Informatyk', '333444555'),
+(9, 'Karolina', 'Mazur', 'Konsultant ds. Płatności', '000111222'),
+(10, 'Tomasz', 'Nowacki', 'Serwisant', '666777888');
 
-UPDATE Lokalizacje
-SET Panstwo = 'Poland'
-WHERE Panstwo = 'Polska';
+INSERT INTO Platnosci (`PlatnoscID`, `Kwota`, `Data`, `TypPlatnosci`)
+VALUES
+(1, 100.50, '2023-11-08', 'Gotówka'),
+(2, 75.20, '2023-11-09', 'Kartą'),
+(3, 120.00, '2023-11-10', 'Gotówka'),
+(4, 90.75, '2023-11-11', 'Kartą'),
+(5, 110.30, '2023-11-12', 'Gotówka'),
+(6, 65.90, '2023-11-13', 'Kartą'),
+(7, 80.60, '2023-11-14', 'Gotówka'),
+(8, 95.45, '2023-11-15', 'Kartą'),
+(9, 105.70, '2023-11-16', 'Gotówka'),
+(10, 70.20, '2023-11-17', 'Kartą');
 
--- Platnosci
+INSERT INTO Oddzialy (`OddzialID`, `LokalizacjaID`, `PracownikID`, `NazwaOddzialu`, `NumerTelOddzialu`)
+VALUES
+(1, 1, 1, 'Oddzial Warszawa', '111111111'),
+(2, 2, 2, 'Oddzial Krakow', '222222222'),
+(3, 3, 3, 'Oddzial Poznan', '333333333'),
+(4, 4, 4, 'Oddzial Wroclaw', '444444444'),
+(5, 5, 5, 'Oddzial Gdansk', '555555555'),
+(6, 6, 6, 'Oddzial Katowice', '666666666'),
+(7, 7, 7, 'Oddzial Lodz', '777777777'),
+(8, 8, 8, 'Oddzial Lublin', '888888888'),
+(9, 9, 9, 'Oddzial Szczecin', '999999999'),
+(10, 10, 10, 'Oddzial Rzeszow', '000000000');
 
-UPDATE Platnosci
-SET Kwota = 150.00
-WHERE PlatnoscID = 3;
+INSERT INTO Wypozyczenie (`WypozyczenieID`, `KlientID`, `PlatnoscID`, `OddzialID`, `DaataOd`, `DataDo`)
+VALUES
+(1, 1, 1, 1, '2023-11-01', '2023-11-10'),
+(2, 2, 2, 2, '2023-11-03', '2023-11-12'),
+(3, 3, 3, 3, '2023-11-05', '2023-11-14'),
+(4, 4, 4, 4, '2023-11-07', '2023-11-16'),
+(5, 5, 5, 5, '2023-11-09', '2023-11-18'),
+(6, 6, 6, 6, '2023-11-11', '2023-11-20'),
+(7, 7, 7, 7, '2023-11-13', '2023-11-22'),
+(8, 8, 8, 8, '2023-11-15', '2023-11-24'),
+(9, 9, 9, 9, '2023-11-17', '2023-11-26'),
+(10, 10, 10, 10, '2023-11-19', '2023-11-28');
 
-UPDATE Platnosci
-SET TypPlatnosci = 'Karta'
-WHERE TypPlatnosci = 'Gotówka';
-
--- Oddzialy
-
-UPDATE Oddzialy
-SET NazwaOddzialu = 'Oddział Gdańsk-Nowy'
-WHERE OddzialID = 5;
-
-UPDATE Oddzialy
-SET NumerTelOddzialu = '111222333'
-WHERE LokalizacjaID = 2;
-
--- Wypozyczenie
-
-UPDATE Wypozyczenie
-SET DataDo = '2023-11-25'
-WHERE WypozyczenieID = 7;
-
-UPDATE Wypozyczenie
-SET KlientID = 2
-WHERE DaataOd < '2023-11-10';
-
--- SamochodWypozyczenie
-
-UPDATE SamochodWypozyczenie
-SET SamochodID = 3
-WHERE SamochodWypozyczenie = 12;
-
-UPDATE SamochodWypozyczenie
-SET WypozyczenieID = 8
-WHERE SamochodID = 6;
-
--- Delete
-
--- Klienci
-
-DELETE FROM Pracownicy WHERE PracownikID = 2;
-DELETE FROM Klienci WHERE NumerTel LIKE '555%';
-
--- Pracownicy
-
-DELETE FROM Samochody WHERE SamochodID = 1;
-DELETE FROM Pracownicy WHERE Stanowisko = 'Recepcjonistka';
-
--- Samochody
-
-DELETE FROM Samochody WHERE SamochodID = 5;
-DELETE FROM Samochody WHERE Silnik = 'Diesel';
-
--- Lokalizacje
-
-DELETE FROM Lokalizacje WHERE LokalizacjaID = 2;
-DELETE FROM Lokalizacje WHERE Miasto LIKE 'W%';
-
--- Platnosci
-
-DELETE FROM Platnosci WHERE PlatnoscID = 7;
-DELETE FROM Platnosci WHERE TypPlatnosci = 'Kartą';
-
--- Oddzialy
-
-DELETE FROM Oddzialy WHERE OddzialID = 3;
-DELETE FROM Oddzialy WHERE PracownikID IS NULL;
-
--- Wypozyczenie
-
-DELETE FROM Wypozyczenie WHERE WypozyczenieID = 6;
-DELETE FROM Wypozyczenie WHERE DaataOd < '2023-11-10';
-
--- SamochodWypozyczenie
-
-DELETE FROM SamochodWypozyczenie WHERE SamochodWypozyczenie = 5;
-DELETE FROM SamochodWypozyczenie WHERE SamochodID = 6;
-
-TRUNCATE TABLE wypozyczenie;
+INSERT INTO SamochodWypozyczenie (`SamochodWypozyczenie`, `WypozyczenieID`, `SamochodID`)
+VALUES
+(1, 1, 1),
+(2, 2, 3),
+(3, 3, 5),
+(4, 4, 2),
+(5, 5, 7),
+(6, 6, 4),
+(7, 7, 10),
+(8, 8, 8),
+(9, 9, 6),
+(10, 10, 9),
+(11, 1, 8),
+(12, 2, 10),
+(13, 3, 7),
+(14, 4, 6),
+(15, 5, 3),
+(16, 6, 1),
+(17, 7, 4),
+(18, 8, 5),
+(19, 9, 2),
+(20, 10, 7);
